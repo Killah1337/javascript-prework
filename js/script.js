bc967bfc8374ef227da4fce84de4717ea21b087e
Herbert Paszkowski <haribofrate@gmail.com>
2022-07-27 23:04:39 +0200

    read computer move

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
+console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'papier';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
+if(randomNumber == 2){
+	computerMove = 'papier';
+  }
+  if(randomNumber == 1){
+	computerMove = 'nożyce';
+  }
 
-if(playerInput == '2'){
-	playerMove = 'papier';
-  }
-  if(playerInput == '3'){
-	playerMove = 'nożyce';
-  }
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
