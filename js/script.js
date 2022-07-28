a9b0e41208445d89f4012ea7a118a50843a2ebcc
Herbert Paszkowski <haribofrate@gmail.com>
2022-07-28 19:50:37 +0200

    result game

--- js/script.js
+++ js/script.js
@@ -32,3 +32,34 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
     playerMove = 'nożyce';
   }
   printMessage('Twój ruch to: ' + playerMove);
+
+  if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrałeś!');
+  }
+  else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygryasz!');
+  }
+  else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  }
+  else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrałeś!');
+  }
+  else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+  else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrałeś!');
+  }
+  else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygryasz!');
+  }
+  else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  }
+  else if( computerMove == 'papier, kamień, nożyce' && playerMove == 'nieznany ruch'){
+    printMessage('Błędna komenda!');
+  }
